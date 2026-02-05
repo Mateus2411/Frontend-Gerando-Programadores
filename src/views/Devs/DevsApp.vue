3f623303bedda93d413633556b4aaf3eed53d3ef
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-02-05 01:44:14 -0300

    Novo direcionamento futuro

---
+++ src/views/Devs/DevsApp.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h1>
+      Ainda não hà nada aqui mas logo logo estará completo
+    </h1>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  font-size: 2rem;
+}
+</style>
