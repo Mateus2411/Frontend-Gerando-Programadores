5a80c1b5dfd58c34021dfbed9cd14cb86c97c7bc
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-02-05 01:44:12 -0300

    FEAT: Utilização de token na api

--- src/axios.js
+++ src/axios.js
@@ -1,30 +1,26 @@
 import axios from 'axios'
 
+// const api = axios.create({
+//   baseURL: 'https://backend-gerando-programadores.onrender.com/api',
+//   withCredentials: true,
+// })
+
 const api = axios.create({
-  baseURL: 'https://backend-gerando-programadores.onrender.com/api/auth',
+  baseURL: 'http://localhost:5000/api',
+  withCredentials: true,
 })
 
-//#region Token Validation
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-    console.log(
-      'Axios interceptor: Adicionando Authorization header:',
-      config.headers.Authorization.substring(0, 50) + '...',
-    )
-  } else {
-    console.log('Axios interceptor: Nenhum token encontrado, header não adicionado')
-  }
-  return config
-})
-
-//#endregion
 // #region Erro Coffee
 
+// axios
+//   .get('https://backend-gerando-programadores.onrender.com/api/coffee')
+//   .then((res) => console.log('Resposta 418:', res.data))
+//   .catch((err) => console.log(err))
+
+
 axios
-  .get('https://backend-gerando-programadores.onrender.com/api/auth/coffee')
+  .get('http://localhost:5000/api/coffee')
   .then((res) => console.log('Resposta 418:', res.data))
   .catch((err) => console.log(err))
 
