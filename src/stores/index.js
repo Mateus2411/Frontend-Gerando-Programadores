ae4b4157fdc58dc00a61d228104047d1201575d7
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-04-25 21:28:01 -0300

    delete: remove arquivos não utilizados e barrel files

--- src/stores/index.js
+++
@@ -1,5 +0,0 @@
-// Exporta todas as stores para facilitar importação
-export { useAuthStore } from './auth'
-export { useThemeStore } from './theme'
-export { useNavigationStore } from './navigation'
-export { useAiStore } from './ai'
