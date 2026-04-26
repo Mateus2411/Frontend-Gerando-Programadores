b9dc16b24dafefc48ba8c1bd86b3257d06f882a9
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-04-25 21:29:13 -0300

    feat: configura registro do service worker para PWA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +9,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+if (import.meta.env.PROD) {
+  registerSW({ immediate: true })
+}
 
 app.mount('#app')
